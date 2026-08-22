8e0e9f6f5008ba065b9ac3dec88b8c63c97c73d8
pashamdhanushreddy <pashamdhanushreddy@gmail.com>
2026-08-22 18:11:42 +0530

    final deployement

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -199,4 +199,9 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-CORS_ALLOW_ALL_ORIGINS = True # Adjust for production
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://pashamdhanushreddy.github.io",
+]
+
