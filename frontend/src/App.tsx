beb83f5f8ba75593967375a0f315243dcccfcb03
pashamdhanushreddy <pashamdhanushreddy@gmail.com>
2026-08-22 18:30:34 +0530

    blank screen issue fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
 import ChatPage from './pages/ChatPage';
 import MemoryPage from './pages/MemoryPage';
